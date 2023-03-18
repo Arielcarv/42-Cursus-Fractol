4f3b34f5d054a87641e85ddfe02939f7a48d4ca8
Ariel Nascimento <ariel.nascimento@americanas.io>
2023-03-18 19:39:00 -0300

    WIP: update Makefile to work on linux

--- fractol.h
+++ fractol.h
@@ -1,23 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fractol.c                                          :+:      :+:    :+:   */
+/*   fractol.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arcarval <arcarval@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/06 23:00:54 by arcarval          #+#    #+#             */
-/*   Updated: 2023/03/18 19:35:15 by arcarval         ###   ########.fr       */
+/*   Created: 2023/03/18 19:18:55 by arcarval          #+#    #+#             */
+/*   Updated: 2023/03/18 19:27:11 by arcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fractol.h"
+#ifdef __APPLE__
+# include "libraries/minilibx-macos/mlx.h"
+#else
+# include "libraries/minilibx-linux/mlx.h"
+#endif
 
-int	main(void)
-{
-	void	*mlx;
-	// void	*mlx_win;
-
-	mlx = mlx_init();
-	// mlx_win = mlx_new_window(mlx, 1280, 1024, "Hello world!");
-	// mlx_loop(mlx);
-}
+#include "Libft/libft.h"
