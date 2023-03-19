0fd9fad6dcbe8d901beb058e423b9a504b71038e
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-03-19 15:24:12 -0300

    WIP: update Makefile to work on linux

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: arcarval <arcarval@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 23:00:54 by arcarval          #+#    #+#             */
-/*   Updated: 2023/03/18 19:35:15 by arcarval         ###   ########.fr       */
+/*   Updated: 2023/03/19 15:23:23 by arcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 int	main(void)
 {
 	void	*mlx;
-	// void	*mlx_win;
+	void	*mlx_win;
 
 	mlx = mlx_init();
-	// mlx_win = mlx_new_window(mlx, 1280, 1024, "Hello world!");
-	// mlx_loop(mlx);
+	mlx_win = mlx_new_window(mlx, 920, 720, "Hello world!");
+	mlx_loop(mlx);
 }
