df7acb61784a203617a20007daed859cfec17688
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-06-07 15:41:11 -0300

    refactor: update fractol.h

--- fractol.h
+++ fractol.h
@@ -6,14 +6,19 @@
 /*   By: arcarval <arcarval@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 19:18:55 by arcarval          #+#    #+#             */
-/*   Updated: 2023/03/18 19:27:11 by arcarval         ###   ########.fr       */
+/*   Updated: 2023/06/07 14:14:57 by arcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef __APPLE__
-# include "libraries/minilibx-macos/mlx.h"
-#else
-# include "libraries/minilibx-linux/mlx.h"
-#endif
+#ifndef FRACTOL_H
+# define FRACTOL_H
 
-#include "Libft/libft.h"
+# ifdef __APPLE__
+#  include "libraries/minilibx-macos/mlx.h"
+# else
+#  include "libraries/minilibx-linux/mlx.h"
+# endif
+
+# include "Libft/libft.h"
+
+#endif
